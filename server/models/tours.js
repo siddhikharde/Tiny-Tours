0ee6aaa44cc3a496cd3c276fd81496d49accca2f
siddhikharde <siddhikharde2005@gmail.com>
2026-01-04 15:03:53 +0530

    Tour model created

---
+++ server/models/tours.js
@@ -0,0 +1,29 @@
+import {model,Schema} from 'mongoose';
+
+const toursSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    cites:{
+        type:[String],
+        default:[],
+    },
+    startDate:{
+        type:Date
+    },
+    endDate:{
+        type:Date,
+    },
+    user:{
+        type:Schema.Type.ObjectId,
+        required:true,
+    }
+})
+
+const Tour=model("Tour", toursSchema);
+export default Tour;
