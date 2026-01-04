fb63e01f7b6183af4622b07c7596add13d7dcecd
siddhikharde <siddhikharde2005@gmail.com>
2026-01-04 23:17:27 +0530

    middleware moved to the seprate folder

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import connectDB from './db.js';
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Tour from "./models/tours.js";
+import { checkJwtToken } from './middleware/jwt.js';
+
 dotenv.config();
 
  const app=express();
@@ -25,20 +27,7 @@ app.get('/health',(req,res)=>{
     })
 })
 
-const checkJwtToken=(req,res,next)=>{
-    const {authorization}=req.headers;
-    const jwtToken=authorization && authorization.split(" ")[1];
-    try{
-        const decode=jwt.verify(jwtToken,process.env.JWT_SECRET);
-        req.user=decode;
-        console.log(decode)
-        next();
-    }catch(e){
-        return res.json({
-            message:"invalid or missing token"
-        })
-    }
-}
+
 
 app.post('/signUp', async (req, res)=>{
     const {name, email, phone, city, country, password}=req.body;
---
+++ server/middleware/jwt.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const checkJwtToken=(req,res,next)=>{
+    const {authorization}=req.headers;
+    const jwtToken=authorization && authorization.split(" ")[1];
+    try{
+        const decode=jwt.verify(jwtToken,process.env.JWT_SECRET);
+        req.user=decode;
+        console.log(decode)
+        next();
+    }catch(e){
+        return res.json({
+            message:"invalid or missing token"
+        })
+    }
+}
+
+export {checkJwtToken};
