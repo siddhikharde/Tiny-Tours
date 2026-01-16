057586506058912bfd6305d25127f9d27d83a99f
siddhikharde <siddhikharde2005@gmail.com>
2026-01-16 23:20:40 +0530

    avtar added to the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,12 +11,19 @@ function Navbar() {
     },[])
   return (
     <div>
-      <div className='bg-[#FFFFFF] flex justify-between items-center px-5'>
+      <div className='bg-[#FFFFFF] flex justify-between items-center px-7'>
       <Link to={"/"}> <img src={logoImg} alt="Tiny Tours"  className='h-18 cursor-pointer'/></Link> 
         <div>
+            
             {userData.name?(
-                <h2 className='felx cursor-pointer items-center justify-center text-xl text-[#0F172A] font-bold'> 
-                {`Hello! ${userData.name.split(" ")[0]}`} <Button title={"Logout"} size="md" variant='danger'/></h2>
+                <div className='flex items-center justify-center gap-2 '>
+                 <div className="text-xl font-bold bg-[#2563EB] cursor-pointer h-10 w-10 flex items-center justify-center rounded-full text-white">
+  {userData?.name?.charAt(0).toUpperCase() || "U"}
+</div>
+                    <h2 className='md:flex cursor-pointer items-center hidden justify-center text-xl text-[#0F172A] font-bold'> 
+                {`Hello! ${userData.name.split(" ")[0]}`} </h2>
+                <Button title={"Logout"} size="md" variant='danger'/>
+                </div>
             ):(
               <Button title={"Login"} variant='primary' size="md" onClick={()=>{
                 navigate("/login");
