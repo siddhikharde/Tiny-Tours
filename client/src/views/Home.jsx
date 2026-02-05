db95becb91ef763e7c910111bb869ced7d871427
siddhikharde <siddhikharde2005@gmail.com>
2026-02-05 17:05:35 +0530

    hero section created

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import bgImg from '../assets/hero-bg.jpg'
+import Button from '../components/Button'
+import { useNavigate } from 'react-router'
 function Home() {
+  const navigate = useNavigate();
   return (
-    <div>
-      <Navbar/>
-      <h1>Home</h1>
+    <div className='bg-[#F8FAFC] min-h-screen'>
+      <Navbar />
+      <div style={{ backgroundImage: `url(${bgImg})` }} className={`bg-[url(${bgImg})] bg-cover bg-center h-[75vh] m-auto  flex  px-5 flex-col gap-5 items-center justify-center bg-gray-900 bg-blend-overlay`}>
+        <h1 className="text-4xl font-bold text-white  text-center ">Share Your Journey. Discover Theirs.</h1>
+        <p className="  text-xl text-gray-300 mt-4 text-center">TripNest is a community where travelers post their tours,
+          share experiences, and inspire others to explore the world.</p>
+        <div className='flex gap-5'>
+          <Button title="Explore Tours" variant='secondary' onClick={() => navigate("/dashboard")} />
+          <Button title="Create Tour" variant='primary' onClick={() => navigate("/tours/new")} />
+        </div>
+      </div>
+
     </div>
   )
 }
