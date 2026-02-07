70b69d1463ad54438586cd6645c1d57820fa2729
siddhikharde <siddhikharde2005@gmail.com>
2026-02-07 22:44:59 +0530

    features cards created

---
+++ client/src/config.js
@@ -0,0 +1,16 @@
+const features=[
+   {
+              title: "Share Your Adventures",
+              desc: "Post tours, upload photos, and document your travel memories.",
+            },
+            {
+              title: "Discover New Places",
+              desc: "Explore trips shared by travelers from all over the world.",
+            },
+            {
+              title: "Community Driven",
+              desc: "Connect with like-minded explorers and get inspired.",
+            },
+]
+
+export {features}
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -4,23 +4,43 @@ import bgImg from '../assets/hero-bg.jpg'
 import Button from '../components/Button'
 import { useNavigate } from 'react-router'
 import Footer from '../components/Footer'
+import { features } from '../config'
 function Home() {
   const navigate = useNavigate();
+
   return (
-    <div className='bg-[#F8FAFC] min-h-screen'>
+    <div className="bg-[#F8FAFC] min-h-screen">
       <Navbar />
-      <div style={{ backgroundImage: `url(${bgImg})` }} className={`bg-[url(${bgImg})] bg-cover bg-center h-[75vh] m-auto  flex  px-5 flex-col gap-5 items-center justify-center bg-gray-900 bg-blend-overlay`}>
-        <h1 className="text-4xl font-bold text-white  text-center ">Share Your Journey. Discover Theirs.</h1>
-        <p className="  text-xl text-gray-300 mt-4 text-center">TripNest is a community where travelers post their tours,
-          share experiences, and inspire others to explore the world.</p>
-        <div className='flex gap-5'>
-          <Button title="Explore Tours" variant='secondary' onClick={() => navigate("/dashboard")} />
-          <Button title="Create Tour" variant='primary' onClick={() => navigate("/tours/new")} />
+      <div
+        style={{ backgroundImage: `url(${bgImg})` }}
+        className="bg-cover bg-center h-[80vh] flex flex-col gap-6 items-center justify-center text-center px-6 bg-black/60 bg-blend-overlay"
+      >
+        <h1 className="text-5xl md:text-6xl font-extrabold text-white max-w-3xl">
+          Share Your Journey. <br />
+          Discover Theirs.
+        </h1>
+
+        <p className="text-lg md:text-xl text-gray-200 max-w-2xl">
+          TripNest is a community where travelers post their tours, share real
+          experiences, and inspire others to explore the world.
+        </p>
+
+        <div className="flex gap-5 mt-6">
+          <Button
+            title="Explore Tours"
+            variant="secondary"
+            onClick={() => navigate("/dashboard")}
+          />
+          <Button
+            title="Create Tour"
+            variant="primary"
+            onClick={() => navigate("/tours/new")}
+          />
         </div>
       </div>
-  <Footer/>
+
     </div>
-  )
+  );
 }
 
 export default Home
